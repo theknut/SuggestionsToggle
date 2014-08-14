2ee0c142459dd7efd607935c18a30b370cb452f1
theknut <alexander.as.schulz@gmail.com>
2014-08-14 23:33:58 +0200

    Add ability to enable suggestions

--- app/src/main/java/de/theknut/suggestionstoggle/XposedMod.java
+++ app/src/main/java/de/theknut/suggestionstoggle/XposedMod.java
@@ -1,5 +1,6 @@
 package de.theknut.suggestionstoggle;
 
+import android.graphics.Rect;
 import android.text.InputType;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -9,53 +10,105 @@ import android.widget.TextView;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 import static de.robv.android.xposed.XposedHelpers.getAdditionalInstanceField;
-import static de.robv.android.xposed.XposedHelpers.removeAdditionalInstanceField;
 import static de.robv.android.xposed.XposedHelpers.setAdditionalInstanceField;
 
 public class XposedMod extends XC_MethodHook implements IXposedHookLoadPackage {
 
+    private static GestureDetector gestureDetector;
     private static String ISHOOKED = "ISHOOKED";
+    private static String INPUTTYPE = "INPUTTYPE";
     private static int NOT_INITIALIZED = -1;
-    private static int MOTION_EVENT = 0;
 
     @Override
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+    public void handleLoadPackage(LoadPackageParam loadPackageParam) throws Throwable {
 
         XposedHelpers.findAndHookMethod(TextView.class, "onTouchEvent", MotionEvent.class, new XC_MethodHook() {
-
-            GestureDetector gestureDetector;
-            EditText editText;
-            int inputType = NOT_INITIALIZED;
+            private int MOTION_EVENT = 0;
 
             @Override
             protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
                 if (!(param.thisObject instanceof EditText)) return;
 
-                if (getAdditionalInstanceField(param.thisObject, ISHOOKED) == null) {
-                    editText = (EditText) param.thisObject;
-                    gestureDetector = new GestureDetector(editText.getContext(), new GestureDetector.SimpleOnGestureListener() {
-                        @Override
-                        public boolean onDoubleTap(MotionEvent e) {
-                            if (inputType == NOT_INITIALIZED) {
-                                inputType = editText.getInputType();
-                                editText.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-                                editText.setSingleLine(false);
-                            } else {
-                                editText.setInputType(inputType);
-                                inputType = NOT_INITIALIZED;
-                                removeAdditionalInstanceField(editText, ISHOOKED);
-                            }
-                            return true;
-                        }
-                    });
-                    setAdditionalInstanceField(editText, ISHOOKED, true);
+                if (gestureDetector != null) {
+                    // detect double tap
+                    gestureDetector.onTouchEvent((MotionEvent) param.args[MOTION_EVENT]);
                 }
-
-                gestureDetector.onTouchEvent((MotionEvent) param.args[MOTION_EVENT]);
             }
         });
+
+        XposedHelpers.findAndHookMethod(TextView.class, "onFocusChanged", boolean.class, Integer.TYPE, Rect.class, new XC_MethodHook() {
+
+            @Override
+            protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
+                if (!(param.thisObject instanceof EditText)) return;
+                boolean hasFocus = (Boolean) param.args[0];
+
+                if (hasFocus) {
+                    // check if we need to hook this EditText
+                    if (getAdditionalInstanceField(param.thisObject, ISHOOKED) == null || !(Boolean) getAdditionalInstanceField(param.thisObject, ISHOOKED)) {
+
+                        final EditText editText = (EditText) param.thisObject;
+                        gestureDetector = new GestureDetector(editText.getContext(), new GestureDetector.SimpleOnGestureListener() {
+
+                            @Override
+                            public boolean onDoubleTap(MotionEvent e) {
+                                int inputType = (Integer) getAdditionalInstanceField(editText, INPUTTYPE);
+                                if (inputType == NOT_INITIALIZED) {
+                                    // save original inputtype
+                                    setAdditionalInstanceField(editText, INPUTTYPE, editText.getInputType());
+
+                                    if (suggestionsDisabled(editText.getInputType())) {
+                                        // enable suggestions if disabled
+                                        editText.setInputType(editText.getInputType() & ~0x80090);
+                                        // keep multiline
+                                        editText.setSingleLine(false);
+                                    } else {
+                                        // disable suggestions if enabled
+                                        editText.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+                                        // keep multiline
+                                        editText.setSingleLine(false);
+                                    }
+                                } else {
+                                    // we are done with this EditText
+                                    unhookView((EditText) param.thisObject);
+                                }
+                                return true;
+                            }
+
+                            private boolean suggestionsDisabled(int inputType) {
+                                // TYPE_TEXT_FLAG_NO_SUGGESTIONS    = 0x80000
+                                // TYPE_TEXT_PASSWORD               = 0x80
+                                // TYPE_TEXT_VISIBLE_PASSWORD       = 0x90
+                                return (inputType & 0x80090) > 0;
+                            }
+                        });
+
+                        // set additional fields to identify the EditText
+                        setAdditionalInstanceField(editText, ISHOOKED, true);
+                        setAdditionalInstanceField(editText, INPUTTYPE, NOT_INITIALIZED);
+                    }
+                } else {
+                    // remove our hook if we lost focus
+                    if (getAdditionalInstanceField(param.thisObject, ISHOOKED) != null && (Boolean) getAdditionalInstanceField(param.thisObject, ISHOOKED)) {
+                        unhookView((EditText) param.thisObject);
+                        gestureDetector = null;
+                    }
+                }
+            }
+        });
+    }
+
+    private static void unhookView(EditText textview) {
+        int inputType = (Integer) getAdditionalInstanceField(textview, INPUTTYPE);
+        if (inputType != NOT_INITIALIZED) {
+            // restore initial inputType
+            textview.setInputType(inputType);
+        }
+
+        setAdditionalInstanceField(textview, INPUTTYPE, NOT_INITIALIZED);
+        setAdditionalInstanceField(textview, ISHOOKED, false);
     }
 }
